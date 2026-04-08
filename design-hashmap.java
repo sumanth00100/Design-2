d4c30ce715e33c645d4f60ee0d99deea4688a69a
sumanth <58482399+sumanth00100@users.noreply.github.com>
2026-04-08 19:04:20 -0400

    implemented hashmap and queue using stacks

---
+++ design-hashmap.java
@@ -0,0 +1,34 @@
+// Time Complexity :
+// Space Complexity :
+// Did this code successfully run on Leetcode :
+// Any problem you faced while coding this :
+
+import java.util.Arrays;
+class MyHashMap {
+
+    int[] hashstorage = new int[1000001];
+    
+    public MyHashMap() {
+        Arrays.fill(hashstorage, -1);
+    }
+    
+    public void put(int key, int value) {
+        hashstorage[key] = value;
+    }
+    
+    public int get(int key) {
+        return hashstorage[key];
+    }
+    
+    public void remove(int key) {
+        hashstorage[key]=-1;
+    }
+}
+
+/**
+ * Your MyHashMap object will be instantiated and called as such:
+ * MyHashMap obj = new MyHashMap();
+ * obj.put(key,value);
+ * int param_2 = obj.get(key);
+ * obj.remove(key);
+ */
